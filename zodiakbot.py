0f32acd28bfd9c40783a5d6e955b3c31cfb85dcc
METIZON <63011952+METIZON@users.noreply.github.com>
2020-11-06 20:49:42 +0200

    Create zodiakbot.py
    zodiakbot for telegram (russian)

---
+++ zodiakbot.py
@@ -0,0 +1,162 @@
+# -*- coding: utf8 -*-
+
+import telebot
+import requests
+from bs4 import BeautifulSoup
+
+# t.me/TrueGoroscope_bot
+
+try:
+    bot = telebot.TeleBot('<TOKEN>')
+
+    markup1 = telebot.types.InlineKeyboardMarkup()
+
+    oven = telebot.types.InlineKeyboardButton(text='♈Овен♈',
+                                              callback_data='oven')
+    telec = telebot.types.InlineKeyboardButton(text='♉Телец♉',
+                                               callback_data='telec')
+    bliznec = telebot.types.InlineKeyboardButton(text='♊Близнец♊',
+                                                 callback_data='bliznec')
+    rak = telebot.types.InlineKeyboardButton(text='♋Рак♋',
+                                             callback_data='rak')
+    lev = telebot.types.InlineKeyboardButton(text='♌Лев♌',
+                                             callback_data='lev')
+    deva = telebot.types.InlineKeyboardButton(text='♍Дева♍',
+                                              callback_data='deva')
+    vesy = telebot.types.InlineKeyboardButton(text='♎Весы♎',
+                                              callback_data='vesy')
+    skorpion = telebot.types.InlineKeyboardButton(text='♏Скорпион♏',
+                                                  callback_data='scorpion')
+    strelec = telebot.types.InlineKeyboardButton(text='♐Стрелец♐',
+                                                 callback_data='strelec')
+    kozerog = telebot.types.InlineKeyboardButton(text='♑Козерог♑',
+                                                 callback_data='kozerog')
+    aqua = telebot.types.InlineKeyboardButton(text='♒Водолей♒',
+                                              callback_data='aqua')
+    fish = telebot.types.InlineKeyboardButton(text='♓Рыбы♓',
+                                              callback_data='fish')
+
+    markup1.add(oven, telec)
+    markup1.add(bliznec, rak)
+    markup1.add(lev, deva)
+    markup1.add(vesy, skorpion)
+    markup1.add(strelec, kozerog)
+    markup1.add(aqua, fish)
+
+    @bot.message_handler(commands=['start'])
+    def start_msg(message):
+        bot.send_message(message.chat.id,
+                         text='Приветствую! Выберите ваш знак:',
+                         reply_markup=markup1)
+
+    @bot.message_handler(content_types=['text'])
+    def send_msg(message):
+        if message.text:
+            bot.send_message(message.chat.id,
+                             'Ссылка на бота: '
+                             't.me/TrueGoroscope_bot')
+
+    @bot.callback_query_handler(func=lambda call: True)
+    def query_handler(call):
+        if call.data == 'oven':
+            res = requests.get(
+                'https://orakul.com/horoscope/astrologic/general/aries/today.html').text
+            soup = BeautifulSoup(res, features="html.parser")
+            soup.encode('utf-8')
+            cases = soup.find("div", {"class": "horoBlock"}).get_text().strip()
+            info = cases.split('Подробнее')
+            bot.send_message(call.message.chat.id, info[0])
+        elif call.data == 'telec':
+            res = requests.get(
+                'https://orakul.com/horoscope/astrologic/general/taurus/today.html').text
+            soup = BeautifulSoup(res, features="html.parser")
+            soup.encode('utf-8')
+            cases = soup.find("div", {"class": "horoBlock"}).get_text().strip()
+            info = cases.split('Подробнее')
+            bot.send_message(call.message.chat.id, info[0])
+        elif call.data == 'bliznec':
+            res = requests.get(
+                'https://orakul.com/horoscope/astrologic/general/gemini/today.html').text
+            soup = BeautifulSoup(res, features="html.parser")
+            soup.encode('utf-8')
+            cases = soup.find("div", {"class": "horoBlock"}).get_text().strip()
+            info = cases.split('Подробнее')
+            bot.send_message(call.message.chat.id, info[0])
+        elif call.data == 'rak':
+            res = requests.get(
+                'https://orakul.com/horoscope/astrologic/general/cancer/today.html').text
+            soup = BeautifulSoup(res, features="html.parser")
+            soup.encode('utf-8')
+            cases = soup.find("div", {"class": "horoBlock"}).get_text().strip()
+            info = cases.split('Подробнее')
+            bot.send_message(call.message.chat.id, info[0])
+        elif call.data == 'lev':
+            res = requests.get(
+                'https://orakul.com/horoscope/astrologic/general/lion/today.html').text
+            soup = BeautifulSoup(res, features="html.parser")
+            soup.encode('utf-8')
+            cases = soup.find("div", {"class": "horoBlock"}).get_text().strip()
+            info = cases.split('Подробнее')
+            bot.send_message(call.message.chat.id, info[0])
+        elif call.data == 'deva':
+            res = requests.get(
+                'https://orakul.com/horoscope/astrologic/general/virgo/today.html').text
+            soup = BeautifulSoup(res, features="html.parser")
+            soup.encode('utf-8')
+            cases = soup.find("div", {"class": "horoBlock"}).get_text().strip()
+            info = cases.split('Подробнее')
+            bot.send_message(call.message.chat.id, info[0])
+        elif call.data == 'vesy':
+            res = requests.get(
+                'https://orakul.com/horoscope/astrologic/general/libra/today.html').text
+            soup = BeautifulSoup(res, features="html.parser")
+            soup.encode('utf-8')
+            cases = soup.find("div", {"class": "horoBlock"}).get_text().strip()
+            info = cases.split('Подробнее')
+            bot.send_message(call.message.chat.id, info[0])
+        elif call.data == 'scorpion':
+            res = requests.get(
+                'https://orakul.com/horoscope/astrologic/general/scorpio/today.html').text
+            soup = BeautifulSoup(res, features="html.parser")
+            soup.encode('utf-8')
+            cases = soup.find("div", {"class": "horoBlock"}).get_text().strip()
+            info = cases.split('Подробнее')
+            bot.send_message(call.message.chat.id, info[0])
+        elif call.data == 'strelec':
+            res = requests.get(
+                'https://orakul.com/horoscope/astrologic/general/sagittarius/today.html').text
+            soup = BeautifulSoup(res, features="html.parser")
+            soup.encode('utf-8')
+            cases = soup.find("div", {"class": "horoBlock"}).get_text().strip()
+            info = cases.split('Подробнее')
+            bot.send_message(call.message.chat.id, info[0])
+        elif call.data == 'kozerog':
+            res = requests.get(
+                'https://orakul.com/horoscope/astrologic/general/capricorn/today.html').text
+            soup = BeautifulSoup(res, features="html.parser")
+            soup.encode('utf-8')
+            cases = soup.find("div", {"class": "horoBlock"}).get_text().strip()
+            info = cases.split('Подробнее')
+            bot.send_message(call.message.chat.id, info[0])
+        elif call.data == 'aqua':
+            res = requests.get(
+                'https://orakul.com/horoscope/astrologic/general/aquarius/today.html').text
+            soup = BeautifulSoup(res, features="html.parser")
+            soup.encode('utf-8')
+            cases = soup.find("div", {"class": "horoBlock"}).get_text().strip()
+            info = cases.split('Подробнее')
+            bot.send_message(call.message.chat.id, info[0])
+        elif call.data == 'fish':
+            res = requests.get(
+                'https://orakul.com/horoscope/astrologic/general/pisces/today.html').text
+            soup = BeautifulSoup(res, features="html.parser")
+            soup.encode('utf-8')
+            cases = soup.find("div", {"class": "horoBlock"}).get_text().strip()
+            info = cases.split('Подробнее')
+            bot.send_message(call.message.chat.id, info[0])
+        else:
+            pass
+
+    bot.polling(none_stop=True)
+except Exception:
+    pass
